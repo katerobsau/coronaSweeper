6a09b3bf1146d3b03d4ffd4293fca7a85041a678
Kate Saunders <katerobinsonsaunders@gmail.com>
2020-11-10 13:03:22 +1000

    Issues publishing with shinyio
    Had to create two parallel app versions

--- app.R
+++ app.R
@@ -22,10 +22,150 @@ default_level = game_levels[default_index]
 default_prob = prob_infections[default_index]
 
 # Load functions
-source("initialisePersonStatuses.r")
-source("produceGameBoard.r")
-source("getNeighbours.r")
-source("updatePersonStatuses.r")
+# source("initialisePersonStatuses.r")
+# A function to initialise the game board
+initialise_person_statuses <- function(I, J, start_num_infections, 
+                                       symptom_lambda, recovery_lambda){
+  
+  # Initialise grid and displayed infection status
+  init_data <- expand.grid(X = 1:I, Y = 1:J)
+  init_data$shown <- rep("S", I*J)
+  
+  # Initialise quarantine state
+  init_data$quarantined <- rep("No", I*J)
+  
+  # Initialise test state
+  init_data$tested <- rep("unknown", I*J)
+  
+  # Randomly initialise the hidden infections
+  init_data$hidden <- rep("S", I*J)
+  initial_infections <- sample(1:(I*J), start_num_infections)
+  init_data$hidden[initial_infections] <- "I"
+  
+  # Initialise the infection time
+  init_data$infection_period <- rep(NA, I*J)
+  init_data$infection_period[initial_infections] <- 0
+  
+  # Randomly initialise the time until symptoms show
+  init_data$symptom_time <- rpois(I*J, lambda = symptom_lambda)
+  
+  # Randomly initialise the time until recovery
+  init_data$recovery_time <- init_data$symptom_time + rpois(I*J, lambda = recovery_lambda)
+  
+  return(init_data)
+  
+}
+
+# source("produceGameBoard.r")
+# A function to plot the game board
+produce_board_plot <- function(plot_df, 
+                               quarantine_labels, quarantine_levels, 
+                               infection_labels, infection_levels,
+                               test_labels, test_levels){
+  
+  plot_df$shown <- factor(plot_df$shown, levels = infection_levels)
+  plot_df$hidden <- factor(plot_df$hidden, levels = infection_levels)
+  plot_df$quarantined <- factor(plot_df$quarantined, levels = quarantine_levels)
+  plot_df$tested <- factor(plot_df$tested, levels = test_levels)
+  
+  pnt_size = 3.5
+  board_plot <- ggplot(plot_df) +
+    geom_point(aes(x = X, y = Y, col = quarantined),
+               shape = 15, size = pnt_size + 2, alpha = 0.8) +
+    scale_color_manual("EXPOSURE STATUS",
+                       labels = quarantine_labels,
+                       values = c("No" = "lightgray", "Yes" = "coral"),
+                       drop = FALSE) +
+    geom_point(aes(X, Y, fill = shown),
+               shape = 21, size = pnt_size) +
+    scale_fill_manual("INFECTION STATUS",
+                      labels = infection_labels,
+                      values = c("S" = "gray", "I" = "red", "R" = "blue"),
+                      drop = FALSE) +
+    geom_point(aes(X, Y, shape = tested), size = pnt_size, stroke = 1) +
+    scale_shape_manual("TEST STATUS",
+                       labels = test_labels,
+                       values = c("unknown" = 1, "tested" = 13),
+                       drop = FALSE) +
+    coord_fixed() +
+    theme_minimal() +
+    theme(axis.text = element_blank(),
+          axis.title = element_blank(),
+          legend.position = "bottom",
+          legend.box = "vertical",
+          legend.title = element_blank(),
+          legend.text = element_text(size = 14),
+          title = element_text(size = 16, hjust = 0.5)) +
+    guides(fill = guide_legend(order=1),
+           col = guide_legend(order=2),
+           shape = guide_legend(order=3))
+  
+  return(board_plot)
+  
+}
+
+# source("getNeighbours.r")
+# A function to get the neighbours
+get_neighbours <- function(i, I, J){
+  # indexing is by column first then rows 
+  row_index = i%%I + I*(i%%I == 0); 
+  col_index = ceiling(i/I)
+  left_nbr   = i - I + (col_index == 1)*(I*J)
+  right_nbr  = i + I - (col_index == J)*(I*J)
+  top_nbr    = i - 1 + (row_index == 1)*I
+  bottom_nbr = i + 1 - (row_index == I)*I
+  # later part of each nbr equation addresses boundary cases
+  # the board wraps like on a taurus
+  nbrs <- c(left_nbr, right_nbr, top_nbr, bottom_nbr)
+  return(nbrs)
+}
+
+# source("updatePersonStatuses.r")
+update_person_statuses <- function(person_data, I, J, 
+                                   infection_prob,
+                                   test_x, test_y){
+  
+  # Increase counter on infection period
+  infectious_cases = (person_data$hidden == "I")
+  person_data$infection_period[infectious_cases] =
+    person_data$infection_period[infectious_cases] + 1
+  
+  # Get neighbours of infected people
+  i = which(person_data$hidden == "I" & 
+              person_data$quarantined == "No")
+  neighbours = get_neighbours(i, I, J)
+  already_quarantined = which(person_data$quarantined == "Yes")
+  exceptions = c(i, already_quarantined)
+  contacts = setdiff(neighbours, exceptions)
+  
+  # Update with new infections
+  new_infections = rbinom(length(contacts), 1, infection_prob)
+  infected_contacts = contacts[which(new_infections == 1)]
+  person_data$hidden[infected_contacts] = "I"
+  person_data$infection_period[infected_contacts] = 0
+  
+  # Reveal those with symptoms
+  known_cases = (person_data$infection_period > person_data$symptom_time)
+  person_data$shown[known_cases] = "I"
+  
+  # Reveal those who recovered
+  recovered_cases = (person_data$infection_period > person_data$recovery_time)
+  person_data$hidden[recovered_cases] = "R"
+  person_data$shown[recovered_cases] = "R"
+  
+  # Reveal status of the person tested
+  vec_ref = (test_y-1)*J + test_x
+  person_data$tested[vec_ref] = "tested"
+  person_data$shown[vec_ref] = person_data$hidden[vec_ref]
+  
+  # Quarantine those exposed
+  i = which(person_data$shown == "I")
+  isolating = c(i, get_neighbours(i, I, J))
+  person_data$quarantined[isolating] = "Yes"
+  
+  return(person_data)
+  
+}
 
 # UI
 ui <- basicPage(
@@ -45,10 +185,12 @@ server <- function(input, output) {
                                    symptom_lambda, recovery_lambda)
   
   # Set probability of infection
-  setup <- reactiveValues(prob = default_prob)
+  level <- reactiveValues(prob = default_prob)
   observeEvent(input$level,{
     i = which(input$level == game_levels)
-    setup$prob = prob_infections[i]
+    level$prob = prob_infections[i]
+    person_data$infections = initialise_person_statuses(I, J, start_num_infections,
+                                                        symptom_lambda, recovery_lambda)
   })
 
   # Set up reactive values
@@ -78,7 +220,7 @@ server <- function(input, output) {
     counter$countervalue <- counter$countervalue + 1
     
     # Simulate new infections and update statuses
-    person_data$infections = update_person_statuses(person_data$infections, I, J, setup$prob,
+    person_data$infections = update_person_statuses(person_data$infections, I, J, level$prob,
                            test_coord$x, test_coord$y)
     
     # Game stats
